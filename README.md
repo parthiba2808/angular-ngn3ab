b95ab94020f7543c872a411b25cc9c1465e3e983
parthiba2808 <parthimanoj38@gmail.com>
2021-09-15 22:00:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngn3ab
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngn3ab)
---
+++ src/app/post.ts
@@ -0,0 +1,10 @@
+export interface Post {
+  id: number;
+  building: string;
+  city: string;
+  state: string;
+  lat: string;
+  long: string;
+  phon: string;
+  email: string;
+}
